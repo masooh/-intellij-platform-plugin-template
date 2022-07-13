11a3f622c69ac58531c6e70c32d008235db1b818
GitHub Action <action@github.com>
2022-07-13 15:52:57 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/masooh/intellijplatformplugintemplate/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.masooh.intellijplatformplugintemplate.services
+
+import com.github.masooh.intellijplatformplugintemplate.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
